cf18fff878d1379b2f54ab61a7dc7527a93e94ea
engessam <96024774+engessam@users.noreply.github.com>
2023-12-11 23:03:00 +0200

    Delete Network_Project directory

--- Network_Project/out/clang-release/src/Coordinator.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/Coordinator.o: Coordinator.cc Coordinator.h \
-  G:/networks/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h
-
-Coordinator.h:
-
-G:/networks/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h:
--- Network_Project/out/clang-release/src/Node1.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/Node1.o: Node1.cc Node1.h \
-  G:/networks/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h
-
-Node1.h:
-
-G:/networks/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h:
