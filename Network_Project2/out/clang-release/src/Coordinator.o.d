45c85fd8861b77486dfba5984469d354be31f3d1
mohamed07abdelaziz <88625570+mohamed07abdelaziz@users.noreply.github.com>
2024-01-11 17:41:55 +0200

    Delete Network_Project2 directory

--- Network_Project2/out/clang-release/src/Coordinator.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/Coordinator.o: Coordinator.cc Coordinator.h \
-  D:/nnnn/omnetpp-6.0.1/include/omnetpp.h
-
-Coordinator.h:
-
-D:/nnnn/omnetpp-6.0.1/include/omnetpp.h:
--- Network_Project2/out/clang-release/src/Node0.o.d
+++
@@ -1,6 +0,0 @@
-../out/clang-release/src/Node0.o: Node0.cc Node0.h \
-  D:/nnnn/omnetpp-6.0.1/include/omnetpp.h
-
-Node0.h:
-
-D:/nnnn/omnetpp-6.0.1/include/omnetpp.h:
